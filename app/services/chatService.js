1bed6c0c584fac1bfd18e2cee0c7fccada3657fd
Rasheeque <rasheeque.c@cabot.loc>
2020-06-02 12:46:19 +0530

    chat customization

--- app/services/chatService.js
+++ app/services/chatService.js
@@ -4,27 +4,38 @@ const { Chat } = require('../model/Chat')
 module.exports = function chatService(server){
     const io = require('socket.io').listen(server);
     console.log("chatservice called");
+    const onlineUsers = []
     io.on("connection", function (socket) {
-        console.log("a user connected")
-        socket.on('join', function (data) { 
-            console.log('join',data.userid)   
-            socket.join(data.userid);
-          });
+        console.log("a user connected",socket.handshake.query.userid)
+        const userObj = new Object();
+        userObj.userId = socket.handshake.query.userid
+        userObj.socketId = socket.id;
+        onlineUsers.push(userObj);
 
         socket.on('MESSAGE_SEND_REQUEST', (body) => {
             console.log(body);
             Chat.saveMessage(body).then(messageData => {
-                // socket.emit('RECEIVE_MESSAGE',messageData)
-                socket.to(messageData.senderId).emit('RECEIVE_MESSAGE', 'messageData');
-                console.log('send to ',messageData.senderId)
-
+                // socket.to(messageData.senderId).emit('RECEIVE_MESSAGE', 'messageData');
+                console.log('send to ',messageData.message.senderId)
+                const findSocket = onlineUsers.find(user => {
+                  return user.userId == messageData.message.receiverId
+                })
+                if(findSocket){
+                  io.to(findSocket.socketId).emit('RECEIVE_MESSAGE', messageData);
+                }
+                
             }).catch(err => {
                 console.log('err',err)
             })
           });
 
-          socket.on('disconnect', function(){ 
-            console.log("disconnected")
+          socket.on('disconnect', function(){
+            onlineUsers.forEach((user,i)=>{
+              if(user.socketId == socket.id){
+                onlineUsers.splice(i,1)
+              }
+            })
+            console.log("disconnected", onlineUsers)
           });
     })
 }
